e4c38c7e450c2b7f508c43ab97d0e6c3716c225b
Elliott Feltham <elliottfeltham98@gmail.com>
2025-11-07 19:15:55 +0000

    Formatting and making spawn point variable

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,27 @@
 extends CharacterBody2D
 
-'''
-@export and @export_range(x,y) are used to create sliders in the Inspector for Player
-'''
 
-@export var walk_speed = 200.0 #now a var, to manage walk deceleration. see below
-@export var jump_force = -400.0 #now also a var, to manage variable jump deceleration. see below
-@export_range(0,1) var deceleration = 0.1 #walking deceleration. see below
-@export_range(0,1) var decelerate_on_jump_release = 0.5 #jump button deceleration on release. see below
+# @export and @export_range(x,y) are used to create sliders in the Inspector for Player
 
-var jump_counter = 0 #counter to manage dbl jumps
+
+@export var walk_speed = 200.0 # now a var, to manage walk deceleration. see below
+@export var jump_force = -400.0 # now also a var, to manage variable jump deceleration. see below
+@export_range(0,1) var deceleration = 0.1 # walking deceleration. see below
+@export_range(0,1) var decelerate_on_jump_release = 0.5 # jump button deceleration on release. see below
+
+
+var spawn_point = Vector2(324,196) # Vector2 implying 2 coordinates. 324 and 196 is players position when starting game
+var jump_counter = 0 # counter to manage dbl jumps
 var max_jump = 2
-var power_active = false #powerup to triple jump 
+var power_active = false # powerup to triple jump 
 
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-func respawn(): #respawn function to set the players position back at starting coordinates of the test enviro
-	self.global_position = Vector2(324,196) 
+func respawn(): # respawn function to set the players position back at starting coordinates of the test enviro
+	self.global_position = spawn_point
 	
-	#Vector2 implying 2 coordinates. 324 and 196 is players position when starting game
+	
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -28,36 +30,36 @@ func _physics_process(delta: float) -> void:
 		
 
 	# Handle jump.
-	if is_on_floor():  #used to reset counter on floor, useful for jumping after falling
+	if is_on_floor():  # used to reset counter on floor, useful for jumping after falling
 		jump_counter = 0
 		
-	if Input.is_action_just_released("jump") and velocity.y < 0: #jump variable on button press. not ENTIRELY sure how this works yet, but i can find video for it to remind us
+	if Input.is_action_just_released("jump") and velocity.y < 0: # jump variable on button press. not ENTIRELY sure how this works yet, but i can find video for it to remind us
 		velocity.y *= decelerate_on_jump_release
 		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		animated_sprite.play("jump") #animation for jumping,!!!NOT WORKING YET!!
+		animated_sprite.play("jump") # animation for jumping,!!!NOT WORKING YET!!
 		velocity.y = jump_force
 		jump_counter = 0
 	if Input.is_action_just_pressed("jump") and jump_counter < max_jump:
 		velocity.y = jump_force
 		jump_counter += 1
-	if power_active == true: #boolean to increase max jump counter by 1, allowing triple jump
+	if power_active == true: # boolean to increase max jump counter by 1, allowing triple jump
 		max_jump = 3
 		
 	
 
 	# Get the input direction and handle the movement/deceleration.
 	
-	#Get input direction: -1, 0, 1
+	# Get input direction: -1, 0, 1
 	var direction := Input.get_axis("left", "right")
 	
-	#Flip sprite
+	# Flip sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
 		
-	#Play animations
+	# Play animations
 	if direction == 0:
 		animated_sprite.play("idle")
 	else:
