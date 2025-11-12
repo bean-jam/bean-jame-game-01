6320706cbc32c8c4f8963b10442a9b3c6c01a5b9
Elliott Feltham <elliottfeltham98@gmail.com>
2025-11-12 16:51:58 +0000

    Respawn powerups and edit tilemap

--- scripts/triple_jump_powerup.gd
+++ scripts/triple_jump_powerup.gd
@@ -1,14 +1,25 @@
 extends Area2D
 
-
+@onready var shape := $CollisionShape2D
 
 func _on_body_entered(body: Node) -> void:
+	if not visible:
+		return
 	if body.name == "Player":
 		var prev_max_jump = body.max_jump # Track what max jump was before powerupe
-		body.max_jump = 3 # Set to triplejump
+		body.max_jump = 3 # Set to triple jump
 		
-		visible = false # Turn invisible whilst timer runs out
+		set_deferred("visible", false)
+		shape.set_deferred("disabled", true)
+		set_deferred("monitoring", false)
+		
+		# Power up timer
 		await get_tree().create_timer(5.0).timeout # After 5 seconds reset
 		body.max_jump = prev_max_jump
-		
-		queue_free()
+		respawn()
+
+
+func respawn() -> void:
+	show() # visible = true
+	shape.disabled = false
+	monitoring = true
