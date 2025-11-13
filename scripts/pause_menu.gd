d2d94c255b915693436dcbcb75fef3271985f133
Elliott Feltham <elliottfeltham98@gmail.com>
2025-11-13 15:13:15 +0000

    Create a pause menu and build the world scene
    Press ESC to open the pause menu

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause") and get_tree().paused == true:
+		resume()
+	elif Input.is_action_just_pressed("pause") and get_tree().paused == false:
+		pause()
+		
+	
+func _on_resume_button_pressed() -> void:
+	resume()
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit
+
+func resume():
+	get_tree().paused = false
+	hide()
+	
+func pause():
+	show()
+	get_tree().paused = true
+	
